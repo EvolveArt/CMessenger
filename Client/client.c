6f0fe0a16266e081ad6f009efd43310d2b09806e
Julien Mastrangelo <mastrangelo.julien@gmail.com>
2020-05-28 13:56:32 +0200

    authentification of user with a username added

--- Client/client.c
+++ Client/client.c
@@ -3,6 +3,8 @@
 
 #define CMD   "client"
 
+void sendUsername(int fd, char* username);
+
 int main(int argc, char *argv[]) {
   int sock, ret;
   struct sockaddr_in *adrServ;
@@ -36,20 +38,26 @@ int main(int argc, char *argv[]) {
   if (ret < 0)
     erreur_IO("connect");
 
-  if (ecrireLigne(sock, username) == -1)
-    erreur_IO("ecriture socket");
-
-  while (!fin) {
-    printf("ligne> ");
-    if (fgets(ligne, LIGNE_MAX, stdin) == NULL)
-      // sortie par CTRL-D
-      fin = VRAI;
-    else {
-      if (ecrireLigne(sock, ligne) == -1)
-        erreur_IO("ecriture socket");
-
-      if (strcmp(ligne, "fin\n") == 0)
+  sendUsername(sock, username);
+  if(strncmp(username, argv[3], LIGNE_MAX) != 0)
+  {
+    printf("%s already used.\n", argv[3]);
+  }
+  else
+  {
+    while (!fin)
+    {
+      printf("ligne> ");
+      if (fgets(ligne, LIGNE_MAX, stdin) == NULL)
+        // sortie par CTRL-D
         fin = VRAI;
+      else {
+        if (ecrireLigne(sock, ligne) == -1)
+          erreur_IO("ecriture socket");
+
+        if (strcmp(ligne, "fin\n") == 0)
+          fin = VRAI;
+      }
     }
   }
 
@@ -57,4 +65,13 @@ int main(int argc, char *argv[]) {
     erreur_IO("fermeture socket");
 
   exit(EXIT_SUCCESS);
+}
+
+void sendUsername(int fd, char* username)
+{
+  if (write(fd, username, sizeof(username)) == -1)
+    erreur_IO("ecriture socket");
+
+  if (read(fd, username, sizeof(username)) == -1)
+    erreur_IO("lecture socket");
 }
